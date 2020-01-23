379351247ee12253b6ea1ddbe869d247411c3582
unknown <olgarozhdestvina@gmail.com>
2020-01-23 15:58:30 +0800

    Added file for calculating BMI

---
+++ BMI.py
@@ -0,0 +1,9 @@
+# This program calculates your Body Mass Index (BMI)
+
+height = float (input("Enter your height in cm:  "))
+weight = float (input("Enter your weight in kg:  "))
+
+squareOfHeight = height ** 2 / 10000
+
+BMI = weight / squareOfHeight
+print("Your BMI is", BMI, "in metres squared")
