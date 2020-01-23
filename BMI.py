5fbb77b3745b9497bc37100b9d698d7570583cc0
unknown <olgarozhdestvina@gmail.com>
2020-01-23 16:41:21 +0800

    Rounded format

--- BMI.py
+++ BMI.py
@@ -6,4 +6,4 @@ weight = float (input("Enter your weight in kg:  "))
 squareOfHeight = height ** 2 / 10000
 
 BMI = weight / squareOfHeight
-print("Your BMI is", BMI)
+print("Your BMI is {:.2f}".format(BMI))
