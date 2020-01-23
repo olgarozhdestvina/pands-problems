8a01046f821a13aea8aaa89bfce7876888413b52
unknown <olgarozhdestvina@gmail.com>
2020-01-23 12:22:11 +0800

    Modified file for calculating tiles and added file-test from WhirlwindTourOfPython

--- ineedtiles.py
+++ ineedtiles.py
@@ -1,16 +1,10 @@
 # this program calulates how many tiles I need
 # when tilling a wall or floor (in m2)
 
-length = float (input ("Enter room length"))
-
-width = float (input ("Enter room width"))
-
-# length = 4.0
-# width = 2.0
+length = float (input ("Enter room length: "))
+width = float (input ("Enter room width: "))
 
 area = length * width
-
 needed = area * 1.05
 
 print("You need", needed, "tiles metres squared.")
-
---
+++ test.py
@@ -0,0 +1,5 @@
+# file: test.py
+print("Running test.py")
+x = 5
+needed = x * 3
+print("Result is", needed)
